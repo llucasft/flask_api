9686afe86c139e4476586203ea21af658198b223
Lucas Ferreira <lucasfteo@gmail.com>
2023-05-02 22:33:17 -0300

    setting the put method to add new users

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
-from flask import Flask
-from flask_restful import Resource, Api
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
 
 app = Flask("flask_api")
 api = Api(app)
 
+parser = reqparse.RequestParser()
+parser.add_argument('name', required=True)
+
 users = {
-    'user1': { 'name': 'João'}
+    'user1': {'name': 'João'},
+    'user2': {'name': 'Marcos'}
 }
 
 class User(Resource):
-    def get(self):
-        return {'Felipe': 'Gay'}
+
+    def get(self, user_id):
+        if user_id == "all":
+            return users
+        return users[user_id]
     
-api.add_resource(User, '/')
+    def put(self, user_id):
+        args = parser.parse_args()
+        new_user = {'name': args['name']}
+        users[user_id] = new_user
+        
+        users[f"user{len(users)+1}"] = new_user
+
+        return {user_id: users[user_id]}, 201
+    
+api.add_resource(User, '/users/<user_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
