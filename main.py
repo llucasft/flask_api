28bb50d6016baac6cf474f9be3259b68f230b66a
Lucas Ferreira <lucasfteo@gmail.com>
2023-05-05 19:18:38 -0300

    database now functioning correctly, adding and querying users

--- main.py
+++ main.py
@@ -1,38 +1,64 @@
-from flask import Flask, request
+from flask import Flask, request, g
 from flask_restful import Resource, Api, reqparse, abort
 import sqlite3
+from sqlite3 import Error
+from werkzeug.local import LocalProxy
 
 app = Flask("flask_api")
 api = Api(app)
-
-users = {
-    1: {"name": "Lucas", "email": "lucas@example.com", "password": "1234"},
-    2: {"name": "Jose", "email": "jose@example.com", "password": "4321"},
-    3: {"name": "Maria", "email": "maria@example.com", "password": "abcd"}
-}
 
 user_post_args = reqparse.RequestParser()
 user_post_args.add_argument("name", type=str, help="Name is required.", required=True)
 user_post_args.add_argument("email", type=str, help="Email is required.", required=True)
 user_post_args.add_argument("password", type=str, help="Password is required.", required=True)
 
+conn = sqlite3.connect('database.db')
+
 class UserList(Resource):
     def get(self):
+        conn = sqlite3.connect('database.db')
+        c = conn.cursor()
+        c.execute('''CREATE TABLE IF NOT EXISTS users
+            (id INTEGER PRIMARY KEY, name TEXT, email TEXT, password TEXT)''')
+        c.execute('SELECT * FROM users')
+        rows = c.fetchall()
+        users = {}
+        for row in rows:
+            users[row[0]] = {"name": row[1], "email": row[2], "password": row[3]}
+        conn.close()
         return users
 
 class User(Resource):
     def get(self, user_id):
-        user_id = int(user_id)
-        return users[user_id]
+        conn = sqlite3.connect('database.db')
+        c = conn.cursor()
+        c.execute('''CREATE TABLE IF NOT EXISTS users
+            (id INTEGER PRIMARY KEY, name TEXT, email TEXT, password TEXT)''')
+        c.execute('SELECT * FROM users WHERE id=?', (user_id,))
+        row = c.fetchone()
+        if row is None:
+            return f"Usuário com id {user_id} não encontrado."
+        user = {"name": row[1], "email": row[2], "password": row[3]}
+        conn.close()
+        return user
     
     def post(self, user_id):
-        user_id = int(user_id)
+        conn = sqlite3.connect('database.db')
+        c = conn.cursor()
+
+        c.execute('''CREATE TABLE IF NOT EXISTS users
+            (id INTEGER PRIMARY KEY, name TEXT, email TEXT, password TEXT)''')
         args = user_post_args.parse_args()
-        if user_id in users:
-            abort(409, "Usuário com id já existente.")
-        users[user_id] = {"name": args["name"], "email": args["email"], "password": args["password"]}    
-        return users[user_id]        
-    
+        c.execute('SELECT * FROM users WHERE id=?', (user_id,))
+        row = c.fetchone()
+        if row:
+            return f"Usuário com id {user_id} já existe."
+        c.execute('INSERT INTO users(id, name, email, password) VALUES (?, ?, ?, ?)', (user_id, args["name"], args["email"], args["password"]))
+        conn.commit()
+        user = {"name": args["name"], "email": args["email"], "password": args["password"]}
+        conn.close()
+        return user
+        
 api.add_resource(User, '/users/<user_id>')
 api.add_resource(UserList, '/users')
 
