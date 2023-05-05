6039589b52384bf9eacd3c0f1c6716026f877082
Lucas Ferreira <lucasfteo@gmail.com>
2023-05-04 22:31:34 -0300

    post and get requests workin properly

--- main.py
+++ main.py
@@ -1,53 +1,40 @@
 from flask import Flask, request
-from flask_restful import Resource, Api, reqparse
+from flask_restful import Resource, Api, reqparse, abort
 import sqlite3
 
 app = Flask("flask_api")
 api = Api(app)
 
-parser = reqparse.RequestParser()
-parser.add_argument('name', required=True)
-parser.add_argument('email', required=True)
-parser.add_argument('password', required=True)
+users = {
+    1: {"name": "Lucas", "email": "lucas@example.com", "password": "1234"},
+    2: {"name": "Jose", "email": "jose@example.com", "password": "4321"},
+    3: {"name": "Maria", "email": "maria@example.com", "password": "abcd"}
+}
 
-conn = sqlite3.connect('users.db')
-c = conn.cursor()
-c.execute('''CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, email TEXT NOT NULL, password TEXT NOT NULL
-)''')
-conn.commit()
+user_post_args = reqparse.RequestParser()
+user_post_args.add_argument("name", type=str, help="Name is required.", required=True)
+user_post_args.add_argument("email", type=str, help="Email is required.", required=True)
+user_post_args.add_argument("password", type=str, help="Password is required.", required=True)
+
+class UserList(Resource):
+    def get(self):
+        return users
 
 class User(Resource):
-
     def get(self, user_id):
-        if user_id == "all":
-            c.execute('''SELECT * FROM users''')
-            users = {}
-            for row in c.fetchall():
-                users[row[0]] = {'name': row[1]}, {'email': row[2]}, {'password': row[3]}
-                conn.close()
-                return users
-        c.execute("SELECT * FROM users WHERE id=?", (user_id,))
-        row = c.fetchone()
-        conn.close()
-        if row:
-            return {row[0]: {'name': row[1], 'email': row[2], 'password': row[3]}}
-        else:
-            return {"message": "User not found"}, 404
+        user_id = int(user_id)
+        return users[user_id]
     
-    def put(self, user_id):
-        args = parser.parse_args()
-        new_user = {'name': args['name'], 'email': args['email'], 'password': args['password']}
-        conn = sqlite3.connect('mydatabase.db')
-        c = conn.cursor()
-        c.execute("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", (args['name'], args['email'], args['password']))
-        new_user_id = c.lastrowid
-        conn.commit()
-        conn.close()
-        
-        return {new_user_id: new_user}, 201
+    def post(self, user_id):
+        user_id = int(user_id)
+        args = user_post_args.parse_args()
+        if user_id in users:
+            abort(409, "Usuário com id já existente.")
+        users[user_id] = {"name": args["name"], "email": args["email"], "password": args["password"]}    
+        return users[user_id]        
     
 api.add_resource(User, '/users/<user_id>')
+api.add_resource(UserList, '/users')
 
 if __name__ == '__main__':
     app.run(debug=True)
